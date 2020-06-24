f651eb4744aafb9c9dcf1ae5b42afa705cb58752
Ariana Mirus <63701400+ariana-mirus20@users.noreply.github.com>
2020-06-24 15:34:41 +1200

    you can now search for a gif, completed

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,32 +8,27 @@ class App extends React.Component {
   }
 
   bringMeAGif = (query) => {
-    request.get('api.giphy.com/v1/gifs/search')
-      .send({
-        'api_key': 'NihSzDWuMozHjhaq01FDhNnDQMIN0mU5',
-        'q': query,
-        'limit': 1,
-        'rating': 'pg-13',
-        'lang': 'en'
-      })
+    request.get('https://api.giphy.com/v1/gifs/search' + '?q=' + query + '&api_key=NihSzDWuMozHJhaq01FDhNnDQMlN0mU5&limit=20')
       .then(res => {
-        const { title, url } = res.body.data
-        this.setState({ title, url })
+        const randomNo = Math.floor(Math.random() * Math.floor(20))
+        this.setState({
+          title: res.body.data[randomNo].title,
+          url: res.body.data[randomNo].images.downsized.url
+        })
       })
   }
 
   render () {
     return (
       <>
-        <h1>{this.title}</h1>
-        <form>
+        <h1>{this.state.title}</h1>
           <label>
             Word:
             <input type="text" name="gif-name" id="gif-name"/>
           </label>
           <button onClick={() => this.bringMeAGif(document.getElementById('gif-name').value)}>Bring me a gif</button>
-          <img src={this.gifURL}></img>
-        </form>
+        
+        <img src={this.state.url} width="400" height="400"></img>
       </>
     )
   }
