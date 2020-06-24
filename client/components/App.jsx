9acacffaf28b19a96378276552933e3d68876992
Johnny <4018196+j-alipate@users.noreply.github.com>
2020-06-24 14:43:53 +1200

    added form and request from component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import request from 'superagent'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  state = {
+    title: 'Find a GIF',
+    url: 'https://via.placeholder.com/150'
+  }
+
+  bringMeAGif = (query) => {
+    request.get('api.giphy.com/v1/gifs/search')
+      .send({
+        'api_key': 'NihSzDWuMozHjhaq01FDhNnDQMIN0mU5',
+        'q': query,
+        'limit': 1,
+        'rating': 'pg-13',
+        'lang': 'en'
+      })
+      .then(res => {
+        const { title, url } = res.body.data
+        this.setState({ title, url })
+      })
+  }
+
+  render () {
+    return (
+      <>
+        <h1>{this.title}</h1>
+        <form>
+          <label>
+            Word:
+            <input type="text" name="gif-name" id="gif-name"/>
+          </label>
+          <button onClick={() => this.bringMeAGif(document.getElementById('gif-name').value)}>Bring me a gif</button>
+          <img src={this.gifURL}></img>
+        </form>
+      </>
+    )
+  }
 }
 
 export default App
